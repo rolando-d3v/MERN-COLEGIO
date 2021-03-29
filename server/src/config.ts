682ce1b89d87beaf6f1509d66ec17548c0dd447f
raidentek <33987212+raidentek@users.noreply.github.com>
2021-03-29 08:02:45 -0500

    red

--- server/src/config.ts
+++ server/src/config.ts
@@ -3,8 +3,11 @@ dotenv.config()
 
 export default {
     port : process.env.PORT  || '4000',
-    dbname : process.env.DB_NAME  || 'cafe',
-    dbhost : process.env.DB_HOST  || 'localhost',
-    user : process.env.DB_USER  || 'a',
-    pass : process.env.DB_PASS  || 'jIjRdwT5uLjmCu2L',
+
+    db: {
+        name : process.env.DB_NAME  || 'cafe',
+        host : process.env.DB_HOST  || 'localhost',
+        user : process.env.DB_USER  || 'a',
+        pass : process.env.DB_PASS  || 'jIjRdwT5uLjmCu2L',
+    }
 }
--- server/src/db.ts
+++ server/src/db.ts
@@ -3,27 +3,24 @@ import config from "./config";
 
 (async () => {
   try {
-    const db = await mongoose.connect(`mongodb+srv://${config.user}:${config.pass}@cafe.8wspk.mongodb.net/${config.dbname}`, {
-    // const db = await mongoose.connect(`mongodb+srv://abraham:jIjRdwT5uLjmCu2L@cafe.8wspk.mongodb.net/cafe`, {
-      useCreateIndex: true,
-      useUnifiedTopology: true,
-      useNewUrlParser: true,
-      useFindAndModify: false,
-      // auth: {
-      //   user: "",
-      //   password: "",
-      // },
-    });
+    const db = await mongoose.connect(
+      `mongodb+srv://${config.db.user}:${config.db.pass}@cafe.8wspk.mongodb.net/${config.db.name}`,
+      {
+        useCreateIndex: true,
+        useUnifiedTopology: true,
+        useNewUrlParser: true,
+        useFindAndModify: false,
+        // auth: {
+        //   user: "",
+        //   password: "",
+        // },
+      }
+    );
 
-    
-
-  
-    console.log(`server db ${db.connection.name} conected`);
-    // console.log(`server db conected` + db);
+    console.log(`server db ${db.connection.name} conected `);
   } catch (err) {
     console.log({ msn: "Error db", err });
   }
 })();
-
 
 //   mongodb+srv://abraham:jIjRdwT5uLjmCu2L@cafe.8wspk.mongodb.net/cafe
