42ee9b358ea23d4e80d2e19e7a309eb59b2a7222
raidentek <33987212+raidentek@users.noreply.github.com>
2021-04-15 08:44:59 -0500

    discord

--- client/src/components/discord/channelInfo/ChannelInfo.tsx
+++ client/src/components/discord/channelInfo/ChannelInfo.tsx
@@ -1,11 +1,11 @@
 import React from "react";
-import * as AiIcons from 'react-icons/ai'
+import * as FaIcons from 'react-icons/fa'
 import "./channelInfo.scss";
 
 const ChannelInfo: React.FC = () => {
   return (
     <div className="channel-info">
-        <AiIcons.AiOutlineNumber className='channel-info__icon'  />
+        <FaIcons.FaHashtag className='channel-info__icon'  />
       <h4 className='channel-info__text' >chat-livre</h4>
       <div className='channel-info__palo'></div>
       <span className='channel-info__text-secon' >Canal aberto para conversas</span>
