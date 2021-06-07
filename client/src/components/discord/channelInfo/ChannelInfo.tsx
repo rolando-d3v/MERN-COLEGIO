ff7d37e5c9db7527eaf94d48116c015aed533e8e
rolando-d3v <landon3388@gmail.com>
2021-06-06 20:10:34 -0500

    discord

--- client/src/components/discord/channelInfo/ChannelInfo.tsx
+++ client/src/components/discord/channelInfo/ChannelInfo.tsx
@@ -6,9 +6,9 @@ const ChannelInfo: React.FC = () => {
   return (
     <div className="channel-info">
         <FaIcons.FaHashtag className='channel-info__icon'  />
-      <h4 className='channel-info__text' >chat-livre</h4>
+      <h4 className='channel-info__text' >chat-live</h4>
       <div className='channel-info__palo'></div>
-      <span className='channel-info__text-secon' >Canal aberto para conversas</span>
+      <span className='channel-info__text-secon' >Channel open talk</span>
     </div>
   );
 };
